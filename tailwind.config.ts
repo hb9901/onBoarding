10d331890f5690de6e7355e2dc6e5e1731765bd9
hb9901 <hb9901@gmail.com>
2024-10-07 17:29:59 +0900

    update: reset.css 적용 및 tailwind config 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
