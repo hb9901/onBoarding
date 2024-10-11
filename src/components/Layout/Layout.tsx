425163010de1b16fd08e92d01b2b9f52df4b634b
hb9901 <hb9901@gmail.com>
2024-10-11 15:48:15 +0900

    fix: 화면 최소 크기 설정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,13 +4,13 @@ import Header from "./Header";
 
 const Layout = () => {
   return (
-    <>
+    <div className="min-w-[400px]">
       <Header />
       <div className="min-h-[calc(100vh-150px)]">
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
