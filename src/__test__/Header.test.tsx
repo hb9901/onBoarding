15529f9c5bb1e5f27de1c11ee2e68b3c1fce873a
hb9901 <hb9901@gmail.com>
2024-10-11 18:20:45 +0900

    docs: Readme.md 수정

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -4,7 +4,8 @@ import Header from "../components/Layout/Header";
 
 describe("Header", () => {
   it("render tasks", async () => {
+    
     const { container } = render(<Header />);
-    expect(container).toHaveTextContent("Yolo");
+    expect(container).toHaveTextContent("로그인");
   });
 });
